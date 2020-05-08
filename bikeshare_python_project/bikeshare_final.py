b3ccbffe63dabb8796e5bd09d289b6f52083275e
TanyaT_homepc <tanya.yt.tan@gmail.com>
2020-05-07 22:29:03 -0700

    updated demo file

--- bikeshare_python_project/bikeshare_final.py
+++ bikeshare_python_project/bikeshare_final.py
@@ -198,7 +198,5 @@ def main():
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
-
-
 if __name__ == "__main__":
 	main()
